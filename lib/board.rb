1c0b70ffb51420ca3ea3bd69517295964cd09188
gpminsight <90690274+gpminsight@users.noreply.github.com>
2022-10-20 15:58:13 +0100

    Added welcome message and adjusted spacing.

--- lib/board.rb
+++ lib/board.rb
@@ -3,6 +3,15 @@ class Board
     @grid = Array.new(6) { Array.new(7, '   ') }
   end
 
+  def welcome_message
+    <<~HEREDOC
+      Welcome to Console Connect Four. First player to connect 4 pieces consecutively (horizontally, vertically or diagonally) wins the game.
+
+      To place a piece, enter a column number (1 to 7).
+      
+    HEREDOC
+  end
+
   def index
     divider = "------------------------------------"
     board_numbers = [[1, 2, 3, 4, 5, 6, 7], [8, 9, 10, 11, 12, 13, 14], [15, 16, 17, 18, 19, 20, 21], [22, 23, 24, 25, 26, 27, 28], [29, 30, 31, 32, 33, 34, 35], [36, 37, 38, 39, 40, 41, 42]]
@@ -22,5 +31,7 @@ class Board
     puts "| #{board_numbers[5][0]} | #{board_numbers[5][1]} | #{board_numbers[5][2]} | #{board_numbers[5][3]} | #{board_numbers[5][4]} | #{board_numbers[5][5]} | #{board_numbers[5][6]} |"
     puts divider
   end
+
+
 end
 
--- lib/game.rb
+++ lib/game.rb
@@ -2,4 +2,6 @@ require_relative 'board'
 
 puts @blank = Board.new
 
+puts @blank.welcome_message
+
 @blank.index
