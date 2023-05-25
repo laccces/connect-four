52ae03e7b8f0b3bc29b5c294051bbb9c64bbd1bf
laccces <en07djc@icloud.com>
2023-05-25 13:57:15 +0100

    Validation for user input

--- connect-four.py
+++ connect-four.py
@@ -1,4 +1,4 @@
-board = [[' ' for _ in range(7)] for _ in range(6)]
+board = [['.' for _ in range(7)] for _ in range(6)]
 
 divider = "-----------------------------"
 
@@ -8,4 +8,15 @@ for row in board:
     print("| {} | {} | {} | {} | {} | {} | {} |".format(*row))
     print(divider)
 print("  1   2   3   4   5   6   7")
+
+def player_one_turn():
+    player_selection = int(input("Where would you like to move? Enter a column from 1 to 7. "))
+    
+    if player_selection < 1 or player_selection > 7:
+        print("Error, please enter a number between 1 and 7.")
+    
+player_one_turn()
+
+#    if player_selection == 1:
+# if player_selection != '.':
 
