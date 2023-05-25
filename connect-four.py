2ce6651131f3dbcb9b8ca3cd386e0e8135ee34d7
laccces <en07djc@icloud.com>
2023-05-25 14:07:57 +0100

    Added more turn logic. Not quite there yet.

--- connect-four.py
+++ connect-four.py
@@ -2,12 +2,15 @@ board = [['.' for _ in range(7)] for _ in range(6)]
 
 divider = "-----------------------------"
 
-print("Here's the board:")
-print(divider)
-for row in board:
-    print("| {} | {} | {} | {} | {} | {} | {} |".format(*row))
+def print_board():
+    print("Here's the board:")
     print(divider)
-print("  1   2   3   4   5   6   7")
+    for row in board:
+        print("| {} | {} | {} | {} | {} | {} | {} |".format(*row))
+        print(divider)
+    print("  1   2   3   4   5   6   7")
+
+print_board()
 
 def player_one_turn():
     player_selection = int(input("Where would you like to move? Enter a column from 1 to 7. "))
@@ -15,8 +18,21 @@ def player_one_turn():
     if player_selection < 1 or player_selection > 7:
         print("Error, please enter a number between 1 and 7.")
     
+    row = 5
+    column = (player_selection - 1)
+
+    while row > 0:
+        if board[row][column] == ".":
+            board[row][column] = "X"
+            row = -1
+        else: row - 1
+
+    
 player_one_turn()
 
-#    if player_selection == 1:
-# if player_selection != '.':
+print_board()
+
+player_one_turn()
+
+print_board()
 
